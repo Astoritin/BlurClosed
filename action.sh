4071cb544133e6df36c22216bb08aca2972dbaba
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-02-19 11:46:30 +0800

    Update action.sh

--- action.sh
+++ action.sh
@@ -3,42 +3,48 @@
 API=$(getprop ro.build.version.sdk)
 blurstate=$(getprop ro.surface_flinger.supports_background_blur)
 
+echo "- Wait for 20 seconds to execute..."
+echo "! WARNING: IF YOU SEE THIS AFTER EXECUTING action.sh"
+echo "! FOR AT LEAST ONCE, PLEASE FORCE CLOSE Magisk APP"
+echo "! IN RECENT TASKS(APPS) AS SOON AS POSSIBLE!"
+echo "! THIS IS MAGISK APP'S BUG AS RESTARTING SYSTEMUI!"
+sleep 20
 if [ "$blurstate" == "0" ] || [ -z "$blurstate" ]; then
-echo "- Enabling blur effects..."
-resetprop persist.sys.sf.disable_blurs false
-resetprop persist.sys.background_blur_supported true
-resetprop disableBlurs false
-resetprop disableBackgroundBlur false
-resetprop enable_blurs_on_windows 1
-resetprop ro.launcher.blur.appLaunch 1
-resetprop ro.sf.blurs_are_caro 0
-resetprop ro.sf.blurs_are_expensive 1
-resetprop ro.surface_flinger.supports_background_blur 1
-if [ "`getprop ro.miui.ui.version.name`" != "" ]; then
-resetprop ro.miui.has_real_blur 1
-fi
-if [ "$API" -ge "31" ]; then
-resetprop windowBlurBehindEnabled true
-resetprop windowBlurBehindRadius 20
-fi
-else
-echo "- Disabling blur effects..."
-resetprop persist.sys.sf.disable_blurs true
-resetprop persist.sys.background_blur_supported false
-resetprop disableBlurs true
-resetprop disableBackgroundBlur true
-resetprop enable_blurs_on_windows 0
-resetprop ro.launcher.blur.appLaunch 0
-resetprop ro.sf.blurs_are_caro 1
-resetprop ro.sf.blurs_are_expensive 0
-resetprop ro.surface_flinger.supports_background_blur 0
-if [ "`getprop ro.miui.ui.version.name`" != "" ]; then
-resetprop ro.miui.has_real_blur 0
-fi
-if [ "$API" -ge "31" ]; then
-resetprop windowBlurBehindEnabled false
-resetprop windowBlurBehindRadius 0
-fi
+    echo "- Enabling blur effects..."
+    resetprop persist.sys.sf.disable_blurs false
+    resetprop persist.sys.background_blur_supported true
+    resetprop disableBlurs false
+    resetprop disableBackgroundBlur false
+    resetprop enable_blurs_on_windows 1
+    resetprop ro.launcher.blur.appLaunch 1
+    resetprop ro.sf.blurs_are_caro 0
+    resetprop ro.sf.blurs_are_expensive 1
+    resetprop ro.surface_flinger.supports_background_blur 1
+    if [ "`getprop ro.miui.ui.version.name`" != "" ]; then
+        resetprop ro.miui.has_real_blur 1
+    fi
+    if [ "$API" -ge "31" ]; then
+        resetprop windowBlurBehindEnabled true
+        resetprop windowBlurBehindRadius 20
+    fi
+    else
+    echo "- Disabling blur effects..."
+    resetprop persist.sys.sf.disable_blurs true
+    resetprop persist.sys.background_blur_supported false
+    resetprop disableBlurs true
+    resetprop disableBackgroundBlur true
+    resetprop enable_blurs_on_windows 0
+    resetprop ro.launcher.blur.appLaunch 0
+    resetprop ro.sf.blurs_are_caro 1
+    resetprop ro.sf.blurs_are_expensive 0
+    resetprop ro.surface_flinger.supports_background_blur 0
+    if [ "`getprop ro.miui.ui.version.name`" != "" ]; then
+        resetprop ro.miui.has_real_blur 0
+    fi
+    if [ "$API" -ge "31" ]; then
+        resetprop windowBlurBehindEnabled false
+        resetprop windowBlurBehindRadius 0
+    fi
 fi
 
 echo "- Restarting SystemUI..."
@@ -59,3 +65,4 @@ do
     killall $launcher
 done
 echo "- All done!"
+exit 0
