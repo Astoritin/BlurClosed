6d40e40a99eb9c7cad8c98a924fc1ff29621e1b7
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-03-18 11:18:34 +0800

    Create post-fs-data.sh

---
+++ module/post-fs-data.sh
@@ -0,0 +1,55 @@
+#!/system/bin/sh
+MODDIR=${0%/*}
+
+API=$(getprop ro.build.version.sdk)
+DEVICE_BRAND=$(getprop ro.product.brand)
+FLYME_VER_CODE=$(getprop ro.build.flyme.version)
+
+if [ -n "$FLYME_VER_CODE" ] && [ $FLYME_VER_CODE -eq 8 ] ; then
+    resetprop -n persist.perf.wm_static_blur false
+    resetprop -n persist.sys.static_blur_mode false
+    resetprop -n persist.vendor.sf.blur.type none
+    resetprop -n persist.sys.disable_blur_view true
+    resetprop -n persist.meizu.gpu_blur 0
+    resetprop -n persist.sys.force_no_blur 0
+fi
+
+case "$DEVICE_BRAND" in
+    "xiaomi")
+        resetprop -n persist.sysui.miui_blur_enabled 0
+        resetprop -n ro.miui.has_blur 0
+        resetprop -n ro.miui.has_real_blur 0
+        resetprop -n ro.miui.backdrop_sampling_enabled 0
+        resetprop -n persist.miui.ui.optimize_blur 0
+        ;;
+    "oneplus")
+        resetprop -n persist.sys.oneplus.blur.enabled 0
+        resetprop -n persist.sys.oplus.ui.blur 0
+        resetprop -n persist.sys.oppo.blur.enable 0
+        ;;
+    "samsung")
+        resetprop -n persist.sys.samsung.blur.disable 1
+        resetprop -n sys.use_frost_effect 0
+        ;;
+esac
+
+if [ "$API" -ge "31" ]; then
+    resetprop -n windowBlurBehindEnabled false
+    resetprop -n windowBlurBehindRadius 0
+    resetprop -n ro.surface_flinger.supports_background_blur 0
+fi
+
+resetprop -n disableBlurs 1
+resetprop -n disableBackgroundBlur 1
+resetprop -n enable_blurs_on_windows 0
+resetprop -n ro.launcher.blur.appLaunch 0
+resetprop -n ro.sf.blurs_are_costly 1
+resetprop -n ro.sf.blurs_are_expensive 1
+resetprop -n ro.surface_flinger.force_disable_blur 1
+resetprop -n persist.sys.sf.disable_blurs 1
+resetprop -n persist.sys.background_blur_supported 0
+resetprop -n persist.sys.background_blur_status_default 0
+resetprop -n persist.sys.background_blur_version 0
+resetprop -n persist.sys.add_blurnoise_supported 0
+resetprop -n persist.sys.enable_third_blur 0
+resetprop -n persist.sys.dynamic_blur_enabled 0
