0c74a5a6c29d4be7cb981b5bc0a5ecfe69973e45
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-03-18 11:17:59 +0800

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -3,8 +3,8 @@ MODDIR=${0%/*}
 
 API="$(getprop ro.build.version.sdk)"
 
-if [[ "$API" -ge "29" ]]; then
-cmd window disable-blur 0
+if [ "$API" -ge "29" ]; then
+    cmd window disable-blur 0
 else
-wm disable-blur 0
+    cmd wm disable-blur 0
 fi
