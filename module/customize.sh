7a263a8915ef804748d4c0ab38cca3b163ef61d6
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-03-18 11:15:30 +0800

    Update customize.sh

--- module/customize.sh
+++ module/customize.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
 SKIPUNZIP=1
-VERIFY_DIR="$TMPDIR/.aa_bc_verify"
+VERIFY_DIR="$TMPDIR/.aa_verify"
 
 MOD_NAME="$(grep_prop name "${TMPDIR}/module.prop")"
 MOD_VER="$(grep_prop version "${TMPDIR}/module.prop") ($(grep_prop versionCode "${TMPDIR}/module.prop"))"
@@ -18,33 +18,14 @@ check_android_api(){
   fi
 }
 
-add_additional_props(){
+if [ ! -d "$VERIFY_DIR" ]; then
+    mkdir -p "$VERIFY_DIR"
+fi
 
-  local API="$(getprop ro.build.version.sdk)"
-  local MIUI_VER_CODE="$(getprop ro.miui.ui.version.code)"
-  local HYPEROS_VER_CODE="$(getprop ro.mi.os.version.code)"
-
-  if [ -n "$MIUI_VER_CODE" ] || [ -n "$HYPEROS_VER_CODE" ]; then
-    logowl "Detect MIUI/HyperOS"
-    echo "ro.miui.backdrop_sampling_enabled=false" >> "$MODPATH/system.prop"
-    echo "ro.miui.has_real_blur=0" >> "$MODPATH/system.prop"
-    echo "ro.miui.has_blur=0" >> "$MODPATH/system.prop"
-    logowl "Set additional props for Xiaomi/Redmi"
-  fi
-
-  if [ "$API" -ge "31" ]; then
-    echo "windowBlurBehindEnabled=false" >> "$MODPATH/system.prop"
-    echo "windowBlurBehindRadius=0" >> "$MODPATH/system.prop"
-    echo "ro.surface_flinger.supports_background_blur=1" >> "$MODPATH/system.prop"
-    logowl "Set additional props for Android 11+"
-  fi
-
-}
-
-logowl "Extract aautilities.sh"
+echo "- Extract aautilities.sh"
 unzip -o "$ZIPFILE" 'aautilities.sh' -d "$TMPDIR" >&2
 if [ ! -f "$TMPDIR/aautilities.sh" ]; then
-  logowl "! Failed to extract aautilities.sh!"
+  echo "! Failed to extract aautilities.sh!"
   abort "! This zip may be corrupted!"
 fi
 
@@ -52,14 +33,16 @@ fi
 
 logowl "Setting up $MOD_NAME"
 logowl "Version: $MOD_VER"
-show_system_info
 install_env_check
 check_android_api
+show_system_info
+logowl "Essential checks"
+extract "$ZIPFILE" 'aautilities.sh' "$VERIFY_DIR"
+extract "$ZIPFILE" 'customize.sh' "$VERIFY_DIR"
 logowl "Extract module files"
 extract "$ZIPFILE" 'module.prop' "$MODPATH"
-extract "$ZIPFILE" 'system.prop' "$MODPATH"
+extract "$ZIPFILE" 'post-fs-data.sh' "$MODPATH"
 extract "$ZIPFILE" 'service.sh' "$MODPATH"
-extract "$ZIPFILE" 'uninstall.sh' "$MODPATH"
-add_additional_props
+rm -rf "$VERIFY_DIR"
 set_module_files_perm
 logowl "Welcome to use ${MODNAME}!"
