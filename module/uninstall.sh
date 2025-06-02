8a27c69e44eb68ab08b787d3232dd724363935f4
Astoritin Ambrosius <162526927+Astoritin@users.noreply.github.com>
2025-06-02 23:24:43 +0800

    Update uninstall.sh

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -5,8 +5,6 @@ API="$(getprop ro.build.version.sdk)"
 
 if [ "$API" -ge "29" ]; then
     cmd window disable-blur 0
-    # settings put secure show_rotation_suggestions 1
-    # settings put secure num_rotation_suggestions_accepted 0
 else
     cmd wm disable-blur 0
 fi
