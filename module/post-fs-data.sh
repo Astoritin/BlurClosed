b0c54ba3c31ba0460129e063108391449b698200
Astoritin <Astoritin@outlook.com>
2026-02-03 22:56:55 +0800

    function: use check_and_resetprop instead of brand judging

--- module/post-fs-data.sh
+++ module/post-fs-data.sh
@@ -2,76 +2,58 @@
 MODDIR=${0%/*}
 
 API="$(getprop ro.build.version.sdk)"
-DEVICE_BRAND="$(getprop ro.product.brand | tr 'A-Z' 'a-z')"
 
-resetprop disableBlurs 1
-resetprop disableBackgroundBlur 1
-resetprop enable_blurs_on_windows 0
-resetprop ro.launcher.blur.appLaunch 0
-resetprop ro.sf.blurs_are_costly 1
-resetprop ro.sf.blurs_are_expensive 1
-resetprop ro.surface_flinger.force_disable_blur 1
-resetprop persist.sys.sf.disable_blurs 1
-resetprop persist.sys.background_blur_supported 0
-resetprop persist.sys.background_blur_status_default 0
-resetprop persist.sys.background_blur_version 0
-resetprop persist.sys.add_blurnoise_supported 0
-resetprop persist.sys.enable_third_blur 0
-resetprop persist.sys.dynamic_blur_enabled 0
+check_and_resetprop() {
+    local prop_name=$1
+    local prop_expect_value=$2
+    local prop_current_value=$(resetprop "$prop_name")
 
-if [ "$API" -ge "31" ]; then
-    resetprop ro.surface_flinger.supports_background_blur 0
-    resetprop windowBlurBehindEnabled false
-    resetprop windowBlurBehindRadius 0
-fi
+    [ -z "$prop_current_value" ] && return 1
 
-if [ "$API" -ge "29" ]; then
-    cmd window disable-blur 1
-else
-    cmd wm disable-blur 1
-fi
+    [ "$prop_current_value" = "$prop_expect_value" ] && return 0
 
-if [ "$DEVICE_BRAND" = "xiaomi" ]; then
+    resetprop -n "$prop_name" "$prop_expect_value"
 
-    miui_ver=$(getprop ro.miui.ui.version.code)
-    hyper_ver=$(getprop ro.mi.os.version.code)
-    
-    if [ "$miui_ver" ] && [ -z "$hyper_ver" ]; then
-        resetprop persist.sysui.miui_blur_enabled 0
-        resetprop ro.miui.has_blur 0
-        resetprop ro.miui.has_real_blur 0
-        resetprop ro.miui.backdrop_sampling_enabled 0
-        resetprop persist.miui.ui.optimize_blur 0
-    fi
+}
 
-elif [ "$DEVICE_BRAND" = "oneplus" ]; then
+check_and_resetprop disableBlurs 1
+check_and_resetprop disableBackgroundBlur 1
+check_and_resetprop enable_blurs_on_windows 0
+check_and_resetprop ro.launcher.blur.appLaunch 0
+check_and_resetprop ro.sf.blurs_are_costly 1
+check_and_resetprop ro.sf.blurs_are_expensive 1
+check_and_resetprop ro.surface_flinger.force_disable_blur 1
+check_and_resetprop persist.sys.sf.disable_blurs 1
+check_and_resetprop persist.sys.background_blur_supported 0
+check_and_resetprop persist.sys.background_blur_status_default 0
+check_and_resetprop persist.sys.background_blur_version 0
+check_and_resetprop persist.sys.add_blurnoise_supported 0
+check_and_resetprop persist.sys.enable_third_blur 0
+check_and_resetprop persist.sys.dynamic_blur_enabled 0
+check_and_resetprop ro.surface_flinger.supports_background_blur 0
+check_and_resetprop windowBlurBehindEnabled false
+check_and_resetprop windowBlurBehindRadius 0
+check_and_resetprop persist.sysui.miui_blur_enabled 0
+check_and_resetprop ro.miui.has_blur 0
+check_and_resetprop ro.miui.has_real_blur 0
+check_and_resetprop ro.miui.backdrop_sampling_enabled 0
+check_and_resetprop persist.miui.ui.optimize_blur 0
+check_and_resetprop persist.sys.oneplus.blur.enabled 0
+check_and_resetprop persist.sys.oplus.ui.blur 0
+check_and_resetprop persist.sys.oppo.blur.enable 0
+check_and_resetprop persist.sys.samsung.blur.disable 1
+check_and_resetprop sys.use_frost_effect 0
+check_and_resetprop persist.perf.wm_static_blur false
+check_and_resetprop persist.sys.static_blur_mode true
+check_and_resetprop persist.vendor.sf.blur.type none
+check_and_resetprop persist.sys.disable_blur_view true
+check_and_resetprop persist.meizu.gpu_blur 0
+check_and_resetprop persist.sys.force_no_blur 0
+check_and_resetprop persist.perf.wm_static_blur false
+check_and_resetprop persist.sys.disable_blur_view true
+check_and_resetprop persist.sys.disable_glass_blur true
+check_and_resetprop persist.sys.static_blur_mode true
 
-    resetprop persist.sys.oneplus.blur.enabled 0
-    resetprop persist.sys.oplus.ui.blur 0
-    resetprop persist.sys.oppo.blur.enable 0
+[ "$API" -ge "29" ] && cmd window disable-blur 1 || cmd wm disable-blur 1
 
-elif [ "$DEVICE_BRAND" = "samsung" ]; then
-
-    resetprop persist.sys.samsung.blur.disable 1
-    resetprop sys.use_frost_effect 0
-
-elif [ "$DEVICE_BRAND" = "meizu" ]; then
-
-    flyme_ver="$(getprop ro.build.flyme.version)"
-
-    if [ "$flyme_ver" -eq 8 ]; then
-        resetprop persist.perf.wm_static_blur false
-        resetprop persist.sys.static_blur_mode true
-        resetprop persist.vendor.sf.blur.type none
-        resetprop persist.sys.disable_blur_view true
-        resetprop persist.meizu.gpu_blur 0
-        resetprop persist.sys.force_no_blur 0
-    elif [ "$flyme_ver" -eq 7 ]; then
-        resetprop persist.perf.wm_static_blur false
-        resetprop persist.sys.disable_blur_view true
-        resetprop persist.sys.disable_glass_blur true
-        resetprop persist.sys.static_blur_mode true
-    fi
-
-fi
 
